d58c5345c8c72ed95affcad22890b2647fe68038
Farhanbwn <farhanbwn2003@gmail.com>
2025-01-30 18:56:58 +0530

    remove user_name and Password

--- jdbc.java
+++ jdbc.java
@@ -4,7 +4,7 @@ public class jdbc {
 public static void main(String[] args) {
     try{
         Class.forName("oracle.jdbc.driver.OracleDriver");
-        Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","L20233080","2436");
+        Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","User_name","Password");
         Statement st =con.createStatement();
         /*st.executeQuery("insert into Student_details values('S0077','Nishan','Bwn','IT','28-oct-03')");
         System.out.println("1 row inserted");*/
